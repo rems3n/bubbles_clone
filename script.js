b4f0fd64c7bd1614869cf98b4f10cc6bc38fd42d
Chris Gusty <cgusty1@gmail.com>
2024-04-13 17:16:14 -0400

    added new sections: icons and demo video

---
+++ script.js
@@ -0,0 +1,5 @@
+$('.carousel').carousel({
+    interval: 2000, // Adjust the interval for icon movement
+    wrap: true     // Set to false if you don't want the carousel to cycle continuously
+  });
+  
